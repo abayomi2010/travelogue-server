9a1c164e0020b9332a2a74b180e69f556a007421
Abayomi Balogun <109353177+abayomi2010@users.noreply.github.com>
2023-03-30 14:35:08 +0100

    minor fixes

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
 
 
 const app = express();
-// dotenv.config();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -17,10 +17,9 @@ app.use(cors());
 app.use("/posts", postRoutes);
 
 
-const CONNECTION_URL = "mongodb+srv://abayomibalogun:labalaba@cluster0.1y6paaw.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
     .catch((error) => console.log(`${error} did not connect`));
 
